2f3dace32207b8c90d505017f09c04c504957880
Leighton Coutinho <coutinho.leighton1@gmail.com>
2023-01-21 17:11:02 -0500

    fixedeverything

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Welcome from './components/welcome';
+
+function App() {
+  return (
+  <div className="App">
+    <Welcome />
+  </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Welcome from './components/welcome';
-
-function index() {
-  return (
-    <div>
-      <Welcome />
-    </div>
-  )
-}
-export default index;
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    <Welcome />
-  </div>
-  );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+<App />
+);
