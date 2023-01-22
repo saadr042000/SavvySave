f30941bc5f8d0cb6b2a2155273d4c85214e070a2
Saad Rahman <saadrahman242@gmail.com>
2023-01-21 19:48:32 -0500

    ok

---
+++ src/components/method1.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom/client';
+export class MEthod1 extends Component {
+    render(){
+        return (
+            <div>
+                <hgroup>
+                <h1>Welcome to SavvySave</h1>
+                <h3>
+                    Based on your answers, your omptimal budgeting should be as follows
+                </h3>
+                <div>
+                    <a class="firstB">Let's start your budgeting</a>
+                </div>
+                </hgroup>
+            </div>
+        )
+    }
+
+}
+export default MEthod1;
