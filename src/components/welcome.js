3d35da3b4d61782f40a19cbf779afa8f1f757349
Leighton Coutinho <coutinho.leighton1@gmail.com>
2023-01-21 19:31:32 -0500

    switched to functional component

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,25 +1,27 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/welcome.css';
-export class Welcome extends Component {
-    render(){
-        return (
-            <div>
-                <hgroup>
-                <h1>Welcome to SavvySave</h1>
-                <h3>
-                    In the next few steps, we will start to personalize your SavvySave
-                    experience.
-                </h3>
-                <div>
-                    <a class="firstB">Let's start your budgeting</a>
-                </div>
-                </hgroup>
-            </div>
-        )
-    }
 
+const Welcome = (props) => {
+    //define use state here
+
+    
+    return (
+        <div>
+        <hgroup>
+        <h1>Welcome to SavvySave</h1>
+        <h3>
+            In the next few steps, we will start to personalize your SavvySave
+            experience.
+        </h3>
+        <div>
+            <a class="firstB">Let's start your budgeting</a>
+        </div>
+        </hgroup>
+    </div>
+    );
 }
+
 export default Welcome;
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
